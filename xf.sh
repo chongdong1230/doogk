a7b78f1b70e3a9f3d946f7d175bc9a61477eb519
chongdong1230 <80104532+chongdong1230@users.noreply.github.com>
2022-03-29 19:01:16 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "c7e8ff42-f4b6-4719-9aab-b519db01acf5",
                         "alterId": 0
                     }
                 ],
